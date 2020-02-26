1bea25ee5892fa5234416ffa5c2b4883f31b407a
Esperz <49089057+esperz2019@users.noreply.github.com>
2020-02-26 20:59:54 +0800

    Comment some predefined variables

--- dev-support/win-paths-eg.cmd
+++ dev-support/win-paths-eg.cmd
@@ -38,11 +38,11 @@ SET MAVEN_OPTS=-Dmaven.repo.local=C:\Tools\m2
 @REM command path
 @REM
 
-SET MAVEN_HOME=C:\Tools\apache-maven-3.5.0
-SET JAVA_HOME=C:\Tools\jdk
+@REM SET MAVEN_HOME=C:\Tools\apache-maven-3.5.0
+@REM SET JAVA_HOME=C:\Tools\jdk
 SET MSVS=C:\Program Files (x86)\Microsoft Visual Studio 12.0
 SET PROTO_BIN=C:\Tools\protobuf-2.5.0
-SET GIT_HOME=C:\Program Files\Git
+@REM SET GIT_HOME=C:\Program Files\Git
 
 SET PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%PROTO_BIN%;%GIT_HOME%\bin;%PATH%
 
